583f2999e7eba12c28fd06435ff05746681e6d13
Andrei-Alexandru07 <andrei_alexandru707@yahoo.com>
2020-08-14 15:06:13 +0300

    Todo list application

---
+++ aJavaProgrammingMasterclassForJavaDevelopers/src/main/java/kJavaFX/eTodoList/DialogController.java
@@ -0,0 +1,29 @@
+package kJavaFX.eTodoList;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import kJavaFX.eTodoList.DataModel.TodoData;
+import kJavaFX.eTodoList.DataModel.TodoItem;
+import java.time.LocalDate;
+
+public class DialogController {
+    @FXML
+    private TextField shortDescriptionField;
+    @FXML
+    private TextArea detailsArea;
+    @FXML
+    private DatePicker deadlinePicker;
+
+    public TodoItem processResults() {
+        String shortDescription = shortDescriptionField.getText().trim();
+        String details = detailsArea.getText().trim();
+        LocalDate deadlineValue = deadlinePicker.getValue();
+
+        TodoItem newItem = new TodoItem(shortDescription, details, deadlineValue);
+        TodoData.getInstance().addTodoItem(newItem);
+
+        return newItem;
+    }
+}
