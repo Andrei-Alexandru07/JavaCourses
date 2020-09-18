35b7b4dccdac6980acd75bc50f8ecd4f6e944d2e
Andrei-Alexandru07 <andrei_alexandru707@yahoo.com>
2020-09-18 23:39:02 +0300

    Introduction to Threads

---
+++ aJavaProgrammingMasterclassForJavaDevelopers/src/main/java/mConcurrencyInJava/aThreads/AnotherThread.java
@@ -0,0 +1,20 @@
+package mConcurrencyInJava.aThreads;
+
+import static mConcurrencyInJava.aThreads.ThreadColor.ANSI_BLUE;
+
+public class AnotherThread extends Thread{
+
+    @Override
+    public void run() {
+        System.out.println(ANSI_BLUE + "Hello from " + currentThread().getName());
+
+        try {
+            Thread.sleep(3000);
+        } catch(InterruptedException e) {
+            System.out.println(ANSI_BLUE + "Another thread wake me up.");
+            return;
+        }
+
+        System.out.println(ANSI_BLUE + "Three seconds have passed and I'm awake.");
+    }
+}
---
+++ aJavaProgrammingMasterclassForJavaDevelopers/src/main/java/mConcurrencyInJava/aThreads/MyRunnable.java
@@ -0,0 +1,11 @@
+package mConcurrencyInJava.aThreads;
+
+import static mConcurrencyInJava.aThreads.ThreadColor.ANSI_RED;
+
+public class MyRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println(ANSI_RED + "Hello from MyRunnable's implementation of run().");
+    }
+}
