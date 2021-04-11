4aa5e591a81ebabdfa1aff14a2b2725ce7272a8a
Andrei-Alexandru07 <andrei_alexandru707@yahoo.com>
2021-04-11 22:00:39 +0300

    Initial Commit

---
+++ aJavaProgrammingMasterclassForJavaDevelopers/src/main/java/oRegularExpressions/aRegularExpressionsIntroduction/Main.java
@@ -0,0 +1,28 @@
+package oRegularExpressions.aRegularExpressionsIntroduction;
+
+public class Main {
+    public static void main(String[] args) {
+        String string = "I am a string. Yes I am.";
+        System.out.println(string);
+
+        String yourString = string.replaceAll("I", "You");
+        System.out.println(yourString);
+
+        String alphanumeric = "abcDeeeF12Ghhiiiijkl99z";
+        System.out.println(alphanumeric.replaceAll(".", "Y"));
+
+        System.out.println(alphanumeric.replaceAll("^abcDeee", "YYY"));
+
+        String alphanumeric2 = "abcDeeeF12GhhadcDeeeiiiijkl99z";
+        System.out.println(alphanumeric2.replaceAll("^abcDeee", "YYY"));
+
+        System.out.println(alphanumeric.matches("^hello"));
+        System.out.println(alphanumeric.matches("^abcDeee"));
+        System.out.println(alphanumeric.matches("abcDeeeF12Ghhiiiijkl99z"));
+
+        System.out.println(alphanumeric.replaceAll("ijkl99z$","THE END"));
+        System.out.println(alphanumeric.replaceAll("[aei]", "X"));
+        System.out.println(alphanumeric.replaceAll("[aei]", "I replaced a letter here"));
+        System.out.println(alphanumeric.replaceAll("[aei][Fj]", "X"));
+    }
+}
