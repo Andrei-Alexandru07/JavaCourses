d5eb12a5743ca131ef0b0bc64cccb3ea563e21ac
Andrei-Alexandru07 <andrei_alexandru707@yahoo.com>
2021-04-12 19:18:40 +0300

    Initial commit

---
+++ aJavaProgrammingMasterclassForJavaDevelopers/src/main/java/pDebuggingAndUnitTesting/bIntroductionToUnitTestingWithJUnit/BankAccountTest.java
@@ -0,0 +1,23 @@
+package pDebuggingAndUnitTesting.bIntroductionToUnitTestingWithJUnit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    @Test
+    void withdraw() {
+        fail("This test has yet to be implemented.");
+    }
+
+    @Test
+    void getBalance() {
+        fail("This test has yet to be implemented.");
+    }
+
+    @Test
+    void deposit() {
+        fail("This test has yet to be implemented.");
+    }
+}
